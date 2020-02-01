7febdc5002bcd2163804c6700d2b4ca39660d599
Janis Kovalevskis <janis.kovalevskis@gmail.com>
2020-02-01 18:02:33 +0200

    data driven testing

--- test/tst_myunittests.cpp
+++ test/tst_myunittests.cpp
@@ -8,40 +8,44 @@ class myunittests : public QObject
     Q_OBJECT
 
 public:
-    myunittests();
-    ~myunittests();
+//    myunittests();
+//    ~myunittests();
 
 private slots:
-    void initTestCase();
-    void cleanupTestCase();
+//    void initTestCase();
+//    void cleanupTestCase();
     void test_case1();
+    void test_case1_data();
 
 };
 
-myunittests::myunittests()
+//myunittests::myunittests()
+//{
+
+//}
+
+//myunittests::~myunittests()
+//{
+
+//}
+
+void myunittests::test_case1_data()
 {
+    QTest::addColumn<QVector<QVariant>>("values");
+    QTest::addColumn<double>("result");
+
+    QTest::newRow("one elem") << QVector<QVariant>{1} << 1.0;
+    QTest::newRow("two elem") << QVector<QVariant>{1,2} << 1.5;
+    QTest::newRow("three elem") << QVector<QVariant>{1,2,3} << 2.0;
 
 }
-
-myunittests::~myunittests()
-{
-
-}
-
-void myunittests::initTestCase()
-{
-
-}
-
-void myunittests::cleanupTestCase()
-{
-
-}
-
 void myunittests::test_case1()
 {
-    auto median1 = calculateMedianValue({1,3,4,5});
-    QVERIFY(median1 == 3.5);
+    QFETCH(QVector<QVariant>, values);
+    QFETCH(double, result);
+
+    auto median1 = calculateMedianValue(values);
+    QVERIFY(median1 == result);
 }
 
 QTEST_APPLESS_MAIN(myunittests)
